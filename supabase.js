13c67db9005fee6cafe6b7723427f0878d42b109
iqbalkhan11296-coder <iqbalkhan11296@gmail.com>
2026-09-16 19:54:02 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "YOUR_NEW_SUPABASE_PROJECT_URL";
+
+const SUPABASE_KEY = "YOUR_NEW_SUPABASE_PUBLISHABLE_KEY";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
